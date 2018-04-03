3391640e574514625d088128168394b5f2df8743
wuxinxin1 <QWER_123>
2018-04-03 13:24:38 +0800

    contract

--- eth-contract-dcc/sol/cert/CertServiceFeeModule.sol
+++ eth-contract-dcc/sol/cert/CertServiceFeeModule.sol
@@ -3,5 +3,4 @@ pragma solidity ^0.4.11;
 contract  CertServiceFeeModule{
     function  apply() public returns(uint256 fee);
     function  getFee()view  public returns(uint256);
-
 }
--- eth-contract-dcc/sol/cert/CertServiceIntf.sol
+++ eth-contract-dcc/sol/cert/CertServiceIntf.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.11;
 
 contract CertServiceIntf {
 
-    function apply(bytes digest1, bytes digest2) public returns (uint256 _orderId);
+    function apply(bytes digest1, bytes digest2,uint256 expired) public returns (uint256 _orderId);
 
     function pass(uint256 orderId, bytes digest1, bytes digest2, uint256 expired) public;
 
