8a6f965454ac81cee301efda5c393ac837b071ba
Sisel Free <lstuian@gmail.com>
2018-06-06 14:43:52 +0800

    android dev: minor debug config

--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/tl/TlWebPageActivity.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/tl/TlWebPageActivity.kt
@@ -1,10 +1,18 @@
 package io.wexchain.android.dcc.tl
 
+import android.os.Bundle
 import com.xxy.maple.tllibrary.activity.TlBrowserActivity
 import com.xxy.maple.tllibrary.app.TlSampleJavascript
 import com.xxy.maple.tllibrary.widget.TlX5WebView
+import io.wexchain.android.common.setWebViewDebuggable
 
 class TlWebPageActivity:TlBrowserActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setWebViewDebuggable()
+        super.onCreate(savedInstanceState)
+    }
+
     override fun getSampleJavascript(
         activity: TlBrowserActivity?,
         webView: TlX5WebView?,
