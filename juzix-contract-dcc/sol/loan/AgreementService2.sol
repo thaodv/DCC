864d798f133d13b187eebedcf6f34c04af7ac43d
wuxinxin1 <2468732919@qq.com>
2018-06-06 10:50:08 +0800

    contract

--- juzix-contract-dcc/sol/loan/AgreementService2.sol
+++ juzix-contract-dcc/sol/loan/AgreementService2.sol
@@ -9,6 +9,8 @@ import "../math/SafeMath.sol";
 contract AgreementService2 is OperatorPermission, AgreementInfo, AgreementInfo2 {
 
     using SafeMath for uint256;
+    using ByteUtils for bytes;
+
     struct Agreement {
         uint256 id;
         uint256 version;
@@ -158,20 +160,22 @@ contract AgreementService2 is OperatorPermission, AgreementInfo, AgreementInfo2 
 
     function finishAgreement(uint256 orderId)  external {
         onlyOperator();
-        innerUpdateStatus(orderId, "FULFILLED",msg.sender);
+        string[] memory expectedStatusArray=new string[](1);
+        expectedStatusArray[0]="CREATED";
+        innerUpdateStatus(orderId, "FULFILLED",msg.sender,expectedStatusArray);
     }
 
-    function updateStatus(uint256 orderId, string status) external {
+    function updateStatus(uint256 orderId, string status,string[] expectedStatusArray) external {
         onlyOperator();
-        innerUpdateStatus(orderId, status,msg.sender);
+        innerUpdateStatus(orderId, status,msg.sender,expectedStatusArray);
     }
 
-    function updateStatus(uint256 orderId, string status,address caller) external {
+    function updateStatus(uint256 orderId, string status,address caller,string[] expectedStatusArray) external {
         onlyOwner();
-        innerUpdateStatus(orderId, status,caller);
+        innerUpdateStatus(orderId, status,caller,expectedStatusArray);
     }
 
-    function innerUpdateStatus(uint256 orderId, string status,address caller) internal {
+    function innerUpdateStatus(uint256 orderId, string status,address caller,string[] expectedStatusArray) internal {
         if (!(orderId > 0)) {
             log("!(orderId > 0)");
             throw;
@@ -187,6 +191,12 @@ contract AgreementService2 is OperatorPermission, AgreementInfo, AgreementInfo2 
         }
 
         Agreement storage agreement = agreements[agreementId];
+        //检查状态
+        if(!(checkStatus(agreement.status,expectedStatusArray))){
+            log("!(checkStatus(agreement.status,expectedStatusArray))");
+            throw;
+        }
+        //修改状态
         agreement.status = status;
         agreementUpdated(agreement.id, agreement.caller, agreement.orderId, agreement.borrower, agreement.status);
     }
@@ -223,6 +233,18 @@ contract AgreementService2 is OperatorPermission, AgreementInfo, AgreementInfo2 
         Agreement storage agreement = agreements[agreementId];
         agreement.repayDigest = repayDigest;
         repayDigestUpdated(agreement.id,agreement.caller,agreement.orderId,agreement.borrower,agreement.repayDigest);
+    }
+
+    function checkStatus(string inputStatus, string[] expectedStatusArray) internal returns (bool){
+        if(expectedStatusArray.length==0){
+            return true;
+        }
+        for (uint256 i = 0; i < expectedStatusArray.length; i++) {
+            if (bytes(inputStatus).bytesEqual(bytes(expectedStatusArray[i]))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     function getAgreementLength() public constant returns (uint256 length) {
