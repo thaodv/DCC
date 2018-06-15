3058568c466a1ec81cbeae979be29be5f741b8e1
wuxinxin1 <2468732919@qq.com>
2018-06-15 11:33:25 +0800

    contract

--- eth-contract-dcc/sol/cert/TagService.sol
+++ eth-contract-dcc/sol/cert/TagService.sol
@@ -2,64 +2,88 @@ pragma solidity ^0.4.2;
 
 import "../ownership/OperatorPermission.sol";
 import "../utils/ByteUtils.sol";
-
 contract TagService is OperatorPermission {
 
     using ByteUtils for bytes;
     mapping(address => mapping( uint256 => string)) public tags;
 
-    mapping(address=>uint256[]) tagKeys;
+    uint256[] public tagKeys;
 
-    string[] tagNames;
+    mapping(uint256=>string) public tagNames;
+
+    uint256 constant TAGNAME_MAXSIZE=50;
+    uint256 constant TAGVALUE_MAXSIZE=10*1024;
 
     event tagAdded(address indexed applicant, string tagName,string tagValue);
 
     function TagService(){
-        uint256 index=tagNames.length;
-        tagNames.push("");
-        tagKeys[0].push(index);
-        tags[0][index]="";
+        tagKeys.push(0);
+        tagNames[0]="";
     }
 
-    function addTag(address add, string tagName,string tagValue)public onlyOperator{
-        require(add!=0);
-        require(bytes(tagName).length>0);
-        require(bytes(tagValue).length>0);
-
-        //判读当前地址是否存在标签，如果存在就更新标签的值，然后退出
-        uint256 index=getExistTagNameIndex(add,tagName);
-        if(getExistTagNameIndex(add,tagName)!=0){
-            tags[add][index]=tagValue;
-            return;
-        }
-
-        //当前地址添加新标签
-        index=tagNames.length;
-        tagNames.push(tagName);
-        tagKeys[add].push(index);
-        tags[add][index]=tagValue;
-
-        tagAdded(add,tagName,tagValue);
-    }
-
-    function getTag(address addr,string tagName) public view returns(address,uint256, string,string){
-        require(addr!=0);
-        require(bytes(tagName).length>0);
-
-        uint256 tagNameIndex= getExistTagNameIndex(addr,tagName);
-        if(tagNameIndex!=0){
-            return(addr,tagNameIndex,tagName,tags[addr][tagNameIndex]);
-        }
-        return(addr,tagNameIndex,tagName,"");
-    }
-
-    function getExistTagNameIndex(address addr,string tagName) internal returns(uint256){
-        uint256[] memory keys= tagKeys[addr];
-        for(uint i=0;i<keys.length;i++){
-            if(bytes(tagName).bytesEqual(bytes(tagNames[keys[i]]))){
-                return keys[i];
+    function getExistedTagNameIndex(string tagName) public view returns(uint256){
+        for(uint256 i=1;i<tagKeys.length;i++){
+            uint256 key=tagKeys[i];
+            if(bytes(tagName).bytesEqual(bytes(tagNames[key]))){
+                return key;
             }
         }
         return 0;
     }
+
+    function addTagName(string tagName) public onlyOperator returns(uint256){
+        require(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE);
+        if(getExistedTagNameIndex(tagName)==0){
+            uint256 index=tagKeys.length;
+            tagKeys.push(index);
+            tagNames[index]=tagName;
+        }
+        return index;
+    }
+
+    function deleteTageName(string tagName)public onlyOperator{
+        require(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE);
+
+        uint256 tagNameIndex= getExistedTagNameIndex(tagName);
+        if(tagNameIndex==0){
+            require(false);
+        }
+        for(uint256 i=tagNameIndex;i<tagKeys.length-1;i++){
+            tagKeys[i]=tagKeys[i+1];
+        }
+
+        tagKeys.length--;
+        delete tagNames[tagNameIndex];
+    }
+
+    function addTag(address add, string tagName,string tagValue)public onlyOperator{
+        require(add!=0);
+        require(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE);
+        require(bytes(tagValue).length>0 && bytes(tagValue).length<=TAGVALUE_MAXSIZE);
+
+        uint256 tagNameIndex= getExistedTagNameIndex(tagName);
+        if(tagNameIndex==0){
+            require(false);
+        }
+        tags[add][tagNameIndex]=tagValue;
+        tagAdded(add,tagName,tagValue);
+    }
+
+    function getTag(address addr,string tagName) public view returns(address _addr,uint256 _tagNameIndex,
+        string _tagName,string _tagValue){
+
+        require(addr!=0);
+        require(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE);
+
+        uint256 tagNameIndex= getExistedTagNameIndex(tagName);
+        if(tagNameIndex==0){
+            require(false);
+        }
+        return(addr,tagNameIndex,tagName,tags[addr][tagNameIndex]);
+    }
+
+    function tagKeysLength()public view returns(uint256){
+        return tagKeys.length;
+    }
+
 }
