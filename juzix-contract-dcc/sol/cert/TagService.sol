0c5cf4c16d9a4cb7e8f3d2ced1c31dbaad5f32a5
wuxinxin1 <2468732919@qq.com>
2018-06-19 13:40:55 +0800

    contract

--- juzix-contract-dcc/sol/cert/TagService.sol
+++ juzix-contract-dcc/sol/cert/TagService.sol
@@ -2,32 +2,34 @@ pragma solidity ^0.4.2;
 
 import "../permission/OperatorPermission.sol";
 import "../utils/ByteUtils.sol";
+
 contract TagService is OperatorPermission {
 
     using ByteUtils for bytes;
-    mapping(address => mapping( uint256 => string)) public tags;
+    mapping(address => mapping(uint256 => string)) public tags;
 
     uint256[] public tagKeys;
 
-    uint256 public tagKeysMaxIndex=1;
+    uint256 public maxTagKey = 1;
 
-    mapping(uint256=>string) public tagNames;
+    mapping(uint256 => string) public tagNames;
 
-    uint256 constant TAGNAME_MAXSIZE=50;
-    uint256 constant TAGVALUE_MAXSIZE=10*1024;
+    uint256 constant TAGNAME_MAXSIZE = 50;
+    uint256 constant TAGVALUE_MAXSIZE = 10 * 1024;
 
-    event tagAdded(address indexed applicant, string tagName,string tagValue);
+    event tagPuted(address indexed applicant, string tagName, string tagValue);
+    event tagDeleted(address indexed applicant, string tagName, string tagValue);
+    event tagNameAdded(uint256 indexed key, string tagName);
+    event tagNameDeleted(uint256 indexed key, string tagName);
 
     function TagService(){
         register("TagServiceModule", "0.0.1.0", "TagService", "0.0.1.0");
-        tagKeys.push(0);
-        tagNames[0]="";
     }
 
-    function addTag(address add, string tagName,string tagValue)public{
+    function putTag(address addr, string tagName, string tagValue) public {
         onlyOperator();
-        if(!(add!=0)){
-            log("!(add!=0)");
+        if(!(addr!=0)){
+            log("!(addr!=0)");
             throw;
         }
         if(!(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE)){
@@ -39,19 +41,41 @@ contract TagService is OperatorPermission {
             throw;
         }
 
-        uint256 tagNameIndex= getExistedTagKey(tagName);
-        if(tagNameIndex==0){
+        var (key,) = getTagKey(tagName);
+        if (key == 0) {
             if(!(false)){
                 log("!(false)");
                 throw;
             }
         }
-        tags[add][tagNameIndex]=tagValue;
-        tagAdded(add,tagName,tagValue);
+        tags[addr][key] = tagValue;
+        tagPuted(addr, tagName, tagValue);
     }
 
-    function getTag(address addr,string tagName) public constant returns(address _addr,uint256 _tagNameIndex,
-        string _tagName,string _tagValue){
+    function deleteTag(address addr, string tagName) public {
+        onlyOperator();
+        if(!(addr!=0)){
+            log("!(addr!=0)");
+            throw;
+        }
+        if(!(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE)){
+            log("!(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE)");
+            throw;
+        }
+
+        var (key,) = getTagKey(tagName);
+        if (key == 0) {
+            if(!(false)){
+                log("!(false)");
+                throw;
+            }
+        }
+        delete tags[addr][key];
+        tagDeleted(addr, tagName, "");
+    }
+
+    function getTag(address addr, string tagName) public constant returns (address _addr,
+        string _tagName, string _tagValue){
 
         if(!(addr!=0)){
             log("!(addr!=0)");
@@ -62,68 +86,70 @@ contract TagService is OperatorPermission {
             throw;
         }
 
-        uint256 tagNameIndex= getExistedTagKey(tagName);
-        if(tagNameIndex==0){
+        var (key,) = getTagKey(tagName);
+        if (key == 0) {
             if(!(false)){
                 log("!(false)");
                 throw;
             }
         }
-        return(addr,tagNameIndex,tagName,tags[addr][tagNameIndex]);
+        return (addr, tagName, tags[addr][key]);
     }
 
     function addTagName(string tagName) public {
-        onlyOperator();
+        onlyOwner();
         if(!(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE)){
             log("!(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE)");
             throw;
         }
-        if(getExistedTagKey(tagName)!=0){
+        var (key,) = getTagKey(tagName);
+        if (key != 0) {
             if(!(false)){
                 log("!(false)");
                 throw;
             }
         }
-        tagKeys.push(tagKeysMaxIndex);
-        tagNames[tagKeysMaxIndex]=tagName;
+        tagKeys.push(maxTagKey);
+        tagNames[maxTagKey] = tagName;
+        tagNameAdded(maxTagKey, tagName);
 
-        tagKeysMaxIndex++;
+        maxTagKey++;
     }
 
-
-    function deleteTageName(string tagName)public {
-        onlyOperator();
+    function deleteTagName(string tagName) public {
+        onlyOwner();
         if(!(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE)){
             log("!(bytes(tagName).length>0 && bytes(tagName).length<=TAGNAME_MAXSIZE)");
             throw;
         }
 
-        uint256 tagNameIndex= getExistedTagKey(tagName);
-        if(tagNameIndex==0){
+        var (key, keyIndex) = getTagKey(tagName);
+        if (key == 0) {
             if(!(false)){
                 log("!(false)");
                 throw;
             }
         }
-        for(uint256 i=tagNameIndex;i<tagKeys.length-1;i++){
-            tagKeys[i]=tagKeys[i+1];
+        for (uint256 i = keyIndex; i < tagKeys.length - 1; i++) {
+            tagKeys[i] = tagKeys[i + 1];
         }
 
         tagKeys.length--;
-        delete tagNames[tagNameIndex];
+        delete tagNames[key];
+        tagNameDeleted(key, tagName);
     }
 
-    function getExistedTagKey(string tagName) internal returns(uint256){
-        for(uint256 i=1;i<tagKeys.length;i++){
-            uint256 key=tagKeys[i];
-            if(bytes(tagName).bytesEqual(bytes(tagNames[key]))){
-                return key;
+    function getTagKey(string tagName) public constant returns (uint256 key, uint256 keyIndex){
+        for (keyIndex = 0; keyIndex < tagKeys.length; keyIndex++) {
+            key = tagKeys[keyIndex];
+            if (bytes(tagName).bytesEqual(bytes(tagNames[key]))) {
+                return (key, keyIndex);
             }
         }
-        return 0;
+        return (0, 0);
     }
 
-    function tagKeysLength()public constant returns(uint256){
+    function tagKeysLength() public constant returns (uint256){
         return tagKeys.length;
     }
 
---
+++ juzix-contract-dcc/sol/juzixtoken/RNSTokenModule.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.2;
+
+import "../sysbase/BaseModule.sol";
+import "../library/LibModule.sol";
+
+contract RNSTokenModule is BaseModule {
+    using LibModule for *;
+
+    LibModule.Module tmpModule;
+
+    string moduleName;
+    string moduleVersion;
+    string moduleName_moduleVersion;
+
+    //模块构造函数
+    function RNSTokenModule() {
+	  //定义模块合约名称
+        string memory moduleName = "RNSTokenModule";
+
+        //定义模块合约名称
+        string memory moduleDesc = "RNSTokenModule";
+
+        //定义模块合约版本号
+        string memory moduleVersion = "0.0.1.0";
+
+	//把合约注册到JUICE链上
+        LibLog.log("register RNSTokenModule");
+        register(moduleName, moduleVersion);
+
+	tmpModule.moduleName = moduleName;
+        tmpModule.moduleVersion = moduleVersion;
+        tmpModule.moduleEnable = 0;
+        tmpModule.moduleDescription = moduleDesc;
+        tmpModule.moduleText = moduleDesc;
+
+        uint nowTime = now * 1000;
+        tmpModule.moduleCreateTime = nowTime;
+        tmpModule.moduleUpdateTime = nowTime;
+
+        tmpModule.moduleCreator = msg.sender;
+
+        tmpModule.icon = "";
+        tmpModule.publishTime = nowTime;
+
+        //把模块合约本身添加到系统的模块管理合约中。这一步是必须的，只有这样，用户的dapp才能调用添加到此模块合约的相关合约。
+        LibLog.log("add RNSTokenModule to SysModule");
+        uint ret = addModule(tmpModule.toJson());
+
+        if (ret != 0) {
+            LibLog.log("add RNSTokenModule to SysModule failed");
+            return;
+        }
+    }
+
+
+}
