4bbdbcc5cbaee310a180f5638a3e974252d87637
Sisel Free <lstuian@gmail.com>
2018-06-26 14:36:26 +0800

    android : add new preset tokens

--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/repo/AssetsRepository.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/repo/AssetsRepository.kt
@@ -179,7 +179,24 @@ class AssetsRepository(
                 DigitalCurrency("QTUM", Chain.publicEthChain, 18, "Qtum", "http://www.wexpass.cn/images/Contractz_icon/qtum@2x.png", "0x9a642d6b3368ddc662CA244bAdf32cDA716005BC"),
                 DigitalCurrency("RED", Chain.publicEthChain, 18, "Red Community", "http://www.wexpass.cn/images/Contractz_icon/red_community_token@2x.png", "0x76960Dccd5a1fe799F7c29bE9F19ceB4627aEb2f"),
                 DigitalCurrency("RUFF", Chain.publicEthChain, 18, "RUFF", "http://www.wexpass.cn/images/Contractz_icon/ruff@2x.png", "0xf278c1ca969095ffddded020290cf8b5c424ace2"),
-                DigitalCurrency("AIDOC", Chain.publicEthChain, 18, "AI Doctor", "http://www.wexpass.cn/images/Contractz_icon/ai_doctor@2x.png", "0x584b44853680ee34a0f337b712a8f66d816df151")
+                DigitalCurrency("AIDOC", Chain.publicEthChain, 18, "AI Doctor", "http://www.wexpass.cn/images/Contractz_icon/ai_doctor@2x.png", "0x584b44853680ee34a0f337b712a8f66d816df151"),
+                DigitalCurrency("ICX", Chain.publicEthChain, 18, "ICON", "http://open.dcc.finance/images/token_icon/ICX@2x.png", "0xb5a5f22694352c15b00323844ad545abb2b11028"),
+                DigitalCurrency("ITC", Chain.publicEthChain, 18, "IOT Chain", "http://open.dcc.finance/images/token_icon/ITC@2x.png", "0x5e6b6d9abad9093fdc861ea1600eba1b355cd940"),
+                DigitalCurrency("KNC", Chain.publicEthChain, 18, "KyberNetwork", "http://open.dcc.finance/images/token_icon/KNC@2x.png", "0xdd974d5c2e2928dea5f71b9825b8b646686bd200"),
+                DigitalCurrency("LINK", Chain.publicEthChain, 18, "ChainLink Token", "http://open.dcc.finance/images/token_icon/LINK@2x.png", "0x514910771af9ca656af840dff83e8264ecf986ca"),
+                DigitalCurrency("LUN", Chain.publicEthChain, 18, "Lunyr", "http://open.dcc.finance/images/token_icon/LUN@2x.png", "0xfa05A73FfE78ef8f1a739473e462c54bae6567D9"),
+                DigitalCurrency("MANA", Chain.publicEthChain, 18, "Decentraland", "http://open.dcc.finance/images/token_icon/MANA@2x.png", "0x0f5d2fb29fb7d3cfee444a200298f468908cc942"),
+                DigitalCurrency("MCO", Chain.publicEthChain, 18, "Monaco", "http://open.dcc.finance/images/token_icon/MCO@2x.png", "0xb63b606ac810a52cca15e44bb630fd42d8d1d83d"),
+                DigitalCurrency("NAS", Chain.publicEthChain, 18, "Nebulas", "http://open.dcc.finance/images/token_icon/NAS@2x.png", "0x5d65D971895Edc438f465c17DB6992698a52318D"),
+                DigitalCurrency("OMG", Chain.publicEthChain, 18, "OmiseGO", "http://open.dcc.finance/images/token_icon/OMG@2x.png", "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07"),
+                DigitalCurrency("QSP", Chain.publicEthChain, 18, "Quantstamp", "http://open.dcc.finance/images/token_icon/QSP@2x.png", "0x99ea4db9ee77acd40b119bd1dc4e33e1c070b80d"),
+                DigitalCurrency("VEN", Chain.publicEthChain, 18, "VenChain", "http://open.dcc.finance/images/token_icon/VEN@2x.png", "0xd850942ef8811f2a866692a623011bde52a462c1"),
+                DigitalCurrency("ZRX", Chain.publicEthChain, 18, "ZRX", "http://open.dcc.finance/images/token_icon/ZRX@2x.png", "0xe41d2489571d322189246dafa5ebde1f4699f498"),
+                DigitalCurrency("BAT", Chain.publicEthChain, 18, "BAT", "http://open.dcc.finance/images/token_icon/BAT@2x.png", "0x0d8775f648430679a709e98d2b0cb6250d2887ef"),
+                DigitalCurrency("CVC", Chain.publicEthChain, 18, "CVC", "http://open.dcc.finance/images/token_icon/CVC@2x.png", "0x5f08b925b2c5bcbfba0febef9ab43f6f84310bdf"),
+                DigitalCurrency("ELF", Chain.publicEthChain, 18, "ELF", "http://open.dcc.finance/images/token_icon/ELF@2x.png", "0xbf2179859fc6d5bee9bf9158632dc51678a4100e"),
+                DigitalCurrency("ENG", Chain.publicEthChain, 18, "Enigma", "http://open.dcc.finance/images/token_icon/ENG@2x.png", "0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4"),
+                DigitalCurrency("GNT", Chain.publicEthChain, 18, "Golem", "http://open.dcc.finance/images/token_icon/GNT@2x.png", "0xa74476443119A942dE498590Fe1f2454d7D4aC0d")
                 //, DigitalCurrency("QUN", Chain.publicEthChain, 18, "QunQun", "http://www.wexpass.cn/images/Contractz_icon/qunqun@2x.png", "0x264dc2dedcdcbb897561a57cba5085ca416fb7b4")
         )
     }
