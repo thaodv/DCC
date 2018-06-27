36742cf58761661a5dc466475e420110547e7b30
chenqingsong <cqssuper@163.com>
2018-06-27 11:26:40 +0800

    add dcc exchange tips

--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/DccExchangeActivity.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/DccExchangeActivity.kt
@@ -1,6 +1,15 @@
 package io.wexchain.android.dcc
 
+import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.content.edit
+import io.wexchain.android.common.Prefs
 import io.wexchain.android.dcc.base.BindActivity
 import io.wexchain.android.common.getViewModel
 import io.wexchain.android.common.navigateTo
@@ -15,20 +24,21 @@ import io.wexchain.digitalwallet.Currencies
 class DccExchangeActivity : BindActivity<ActivityDccExchangeBinding>() {
     override val contentLayoutId: Int = R.layout.activity_dcc_exchange
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initToolbar()
         binding.vm = getViewModel()
         binding.cardPublic.root.setOnClickListener {
             val dccPublic = MultiChainHelper.dispatch(Currencies.DCC).first { it.chain == Chain.publicEthChain }
-            navigateTo(DigitalCurrencyActivity::class.java){
+            navigateTo(DigitalCurrencyActivity::class.java) {
                 putExtra(Extras.EXTRA_DIGITAL_CURRENCY, dccPublic)
                 putExtra(Extras.EXTRA_DC_SELECTED, true)
             }
         }
         binding.cardJuzix.root.setOnClickListener {
             val dccJuzix = MultiChainHelper.dispatch(Currencies.DCC).first { it.chain == Chain.JUZIX_PRIVATE }
-            navigateTo(DigitalCurrencyActivity::class.java){
+            navigateTo(DigitalCurrencyActivity::class.java) {
                 putExtra(Extras.EXTRA_DIGITAL_CURRENCY, dccJuzix)
                 putExtra(Extras.EXTRA_DC_SELECTED, true)
             }
@@ -38,6 +48,42 @@ class DccExchangeActivity : BindActivity<ActivityDccExchangeBinding>() {
         }
         binding.ibPublicToDcc.setOnClickListener {
             toast("该功能将在下个版本中开放")
+        }
+        val sp = getSharedPreferences("setting", Context.MODE_PRIVATE)
+        if (sp.getBoolean("first_into", true)) {
+            sp.edit().putBoolean("first_into",false).commit()
+            tipsDialog.show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_dcc_exchange, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_question -> {
+                tipsDialog.show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private val tipsDialog by lazy { TipsDialog() }
+
+    private inner class TipsDialog : Dialog(this, R.style.FullWidthDialog) {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.dialog_dcc_exchange_tips)
+            setCancelable(false)
+            window.attributes.height = ViewGroup.LayoutParams.MATCH_PARENT
+            window.attributes.width = ViewGroup.LayoutParams.MATCH_PARENT
+            setCanceledOnTouchOutside(false)
+            findViewById<View>(R.id.btn_ok).setOnClickListener {
+                dismiss()
+            }
         }
     }
 }
