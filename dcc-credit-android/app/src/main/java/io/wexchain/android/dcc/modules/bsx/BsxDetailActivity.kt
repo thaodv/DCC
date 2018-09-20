ccaff2061b11e20a6805daa1abe2cdf9ab24ee97
zgjsntwangpeng <zgjsntwangpeng@sina.cn>
2018-09-20 16:10:10 +0800

    币生息

--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/bsx/BsxDetailActivity.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/bsx/BsxDetailActivity.kt
@@ -3,15 +3,16 @@ package io.wexchain.android.dcc.modules.bsx
 import android.os.Bundle
 import io.wexchain.android.dcc.base.BindActivity
 import io.wexchain.dcc.R
-import io.wexchain.dcc.databinding.ActivityBsxHoldingBinding
+import io.wexchain.dcc.databinding.ActivityBsxDetailBinding
 
-class BsxHoldingActivity : BindActivity<ActivityBsxHoldingBinding>() {
+class BsxDetailActivity : BindActivity<ActivityBsxDetailBinding>() {
 
     override val contentLayoutId: Int
-        get() = R.layout.activity_bsx_holding
+        get() = R.layout.activity_bsx_detail
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initToolbar(true)
+
     }
 }
