bbbf9528c58d5b91b488b6e10915c84fe8d1b51e
张君君 <649283213@qq.com>
2018-09-29 13:34:46 +0800

    baseCell and base tabbar

---
+++ dcc-credit-iOS/WeXBlockChain/Base/View/WeXBaseTabbar.h
@@ -0,0 +1,17 @@
+//
+//  WeXBaseTabbar.h
+//  WeXBlockChain
+//
+//  Created by 张君君 on 2018/9/27.
+//  Copyright © 2018年 WeX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WeXBaseTabbar : UITabBar
+
+@end
+
+NS_ASSUME_NONNULL_END
