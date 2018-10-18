40ba9d734e98f21d544526f13305e6b8c0a63f75
zgjsntwangpeng <zgjsntwangpeng@sina.cn>
2018-10-18 15:34:37 +0800

    cashloan

---
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/cashloan/CashLoanRepaymentActivity.kt
@@ -0,0 +1,13 @@
+package io.wexchain.android.dcc.modules.cashloan
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.wexchain.dcc.R
+
+class CashLoanRepaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cash_loan_repayment)
+    }
+}
