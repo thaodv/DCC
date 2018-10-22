f2414627b23ef10ca1a6c7a01b815685996667d2
zgjsntwangpeng <zgjsntwangpeng@sina.cn>
2018-10-22 10:47:18 +0800

    cashloan

--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/cashloan/CashLoanRepaymentActivity.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/cashloan/CashLoanRepaymentActivity.kt
@@ -1,13 +1,17 @@
 package io.wexchain.android.dcc.modules.cashloan
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import io.wexchain.android.dcc.base.BindActivity
 import io.wexchain.dcc.R
+import io.wexchain.dcc.databinding.ActivityCashLoanRepaymentBinding
 
-class CashLoanRepaymentActivity : AppCompatActivity() {
+class CashLoanRepaymentActivity : BindActivity<ActivityCashLoanRepaymentBinding>() {
+
+    override val contentLayoutId: Int
+        get() = R.layout.activity_cash_loan_repayment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cash_loan_repayment)
+        initToolbar()
     }
 }
