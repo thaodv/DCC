a493106bc89f1cbc03e68d7f6f71397bbb61e0cb
zgjsntwangpeng <zgjsntwangpeng@sina.cn>
2018-11-13 16:29:02 +0800

    delete node4

--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/fragment/home/DigitalAssetsFragment.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/fragment/home/DigitalAssetsFragment.kt
@@ -1,15 +1,20 @@
 package io.wexchain.android.dcc.fragment.home
 
 import android.arch.lifecycle.Observer
+import com.wexmarket.android.network.Networking
+import io.reactivex.rxkotlin.subscribeBy
 import io.wexchain.android.common.base.BindFragment
 import io.wexchain.android.common.getViewModel
 import io.wexchain.android.common.navigateTo
 import io.wexchain.android.common.onClick
-import io.wexchain.android.dcc.modules.trans.activity.DccExchangeActivity
+import io.wexchain.android.dcc.App
 import io.wexchain.android.dcc.DigitalCurrencyActivity
 import io.wexchain.android.dcc.SearchDigitalCurrencyActivity
 import io.wexchain.android.dcc.constant.Extras
 import io.wexchain.android.dcc.modules.selectnode.SelectNodeActivity
+import io.wexchain.android.dcc.modules.trans.activity.DccExchangeActivity
+import io.wexchain.android.dcc.tools.LogUtils
+import io.wexchain.android.dcc.tools.ShareUtils
 import io.wexchain.android.dcc.view.adapter.ItemViewClickListener
 import io.wexchain.android.dcc.view.adapters.DigitalAssetsAdapter
 import io.wexchain.android.dcc.vm.DigitalAssetsVm
@@ -19,12 +24,15 @@ import io.wexchain.dcc.databinding.ActivityDigitalAssetsBinding
 import io.wexchain.digitalwallet.Chain
 import io.wexchain.digitalwallet.Currencies
 import io.wexchain.digitalwallet.DigitalCurrency
+import io.wexchain.digitalwallet.api.EthJsonRpcApiWithAuth
+import io.wexchain.digitalwallet.proxy.EthsRpcAgent
+import io.wexchain.ipfs.utils.io_main
 import java.math.BigDecimal
 
 /**
  *Created by liuyang on 2018/9/27.
  */
-class DigitalAssetsFragment :BindFragment<ActivityDigitalAssetsBinding>(), ItemViewClickListener<DigitalCurrency> {
+class DigitalAssetsFragment : BindFragment<ActivityDigitalAssetsBinding>(), ItemViewClickListener<DigitalCurrency> {
 
     private var tmpList: List<DigitalCurrency>? = null
     private val adapter = DigitalAssetsAdapter(this)
@@ -34,6 +42,33 @@ class DigitalAssetsFragment :BindFragment<ActivityDigitalAssetsBinding>(), ItemV
 
     override fun onResume() {
         super.onResume()
+
+        var base = ShareUtils.getString(Extras.SP_SELECTED_NODE, App.get().nodeList[0].url)
+
+        if (base == "https://ethrpc4.wexfin.com:58545/") {
+            base = App.get().nodeList[0].url
+        }
+
+        val createApi = Networking(App.get()).createApi(EthJsonRpcApiWithAuth::class.java, base)
+        val agent = EthsRpcAgent.by(createApi)
+
+        val startTime = System.currentTimeMillis()
+        LogUtils.i("SelectNodeActivity-base1-time-start", startTime)
+        agent.checkNode()
+                .io_main()
+                .subscribeBy(
+                        onSuccess = {
+                            if (System.currentTimeMillis() - startTime <= 300) {
+                                binding.digitalSelectNode.setImageResource(R.drawable.img_node_green)
+                            } else if (System.currentTimeMillis() - startTime <= 1000) {
+                                binding.digitalSelectNode.setImageResource(R.drawable.img_node_yellow)
+                            } else {
+                                binding.digitalSelectNode.setImageResource(R.drawable.img_node_red)
+                            }
+                        },
+                        onError = {
+                            binding.digitalSelectNode.setImageResource(R.drawable.img_node_red)
+                        })
 
         val assetsVm = getViewModel<DigitalAssetsVm>()
         assetsVm.ensureHolderAddress(this)
@@ -45,16 +80,16 @@ class DigitalAssetsFragment :BindFragment<ActivityDigitalAssetsBinding>(), ItemV
         assetsVm.filterEvent.observe(this, Observer {
             val b = assetsVm.assetsFilter.get()!!
             assetsVm.assetsFilter.set(!b)
-            if (b){
+            if (b) {
                 adapter.setList(tmpList)
-            }else{
+            } else {
                 val tmp = mutableListOf<DigitalCurrency>()
                 tmpList!!.forEach {
                     val hoding = assetsVm.holding[it]
                     val balanceStr = ViewModelHelper.getBalanceStr(it, hoding)
                     if (balanceStr != "--" && balanceStr.isNotEmpty()) {
                         val value = BigDecimal(balanceStr)
-                        if (value.compareTo(BigDecimal.ZERO) != 0){
+                        if (value.compareTo(BigDecimal.ZERO) != 0) {
                             tmp.add(it)
                         }
                     }
--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/selectnode/SelectNodeActivity.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/selectnode/SelectNodeActivity.kt
@@ -2,6 +2,7 @@ package io.wexchain.android.dcc.modules.selectnode
 
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
@@ -9,6 +10,7 @@ import android.text.style.ForegroundColorSpan
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import com.wexmarket.android.network.Networking
+import io.reactivex.rxkotlin.subscribeBy
 import io.wexchain.android.common.base.BaseCompatActivity
 import io.wexchain.android.dcc.App
 import io.wexchain.android.dcc.constant.Extras
@@ -17,16 +19,37 @@ import io.wexchain.android.dcc.tools.ShareUtils
 import io.wexchain.dcc.R
 import io.wexchain.digitalwallet.api.EthJsonRpcApiWithAuth
 import io.wexchain.digitalwallet.proxy.EthsRpcAgent
-import io.wexchain.ipfs.utils.doMain
+import io.wexchain.ipfs.utils.io_main
 
 class SelectNodeActivity : BaseCompatActivity() {
-
 
     lateinit var grContent: RadioGroup
     lateinit var cb1: RadioButton
     lateinit var cb2: RadioButton
     lateinit var cb3: RadioButton
-    lateinit var cb4: RadioButton
+    //lateinit var cb4: RadioButton
+
+    private lateinit var green: Drawable
+    private lateinit var yellow: Drawable
+    private lateinit var red: Drawable
+
+    private val nodeList: List<NodeBean>
+        get() {
+            val nodeList = App.get().nodeList
+            return nodeList
+        }
+
+    var index: Int = 1
+
+    lateinit var base1: String
+    lateinit var base2: String
+    lateinit var base3: String
+    //lateinit var base4: String
+
+    var text1 = ""
+    var text2 = ""
+    var text3 = ""
+    //var text4 = ""
 
     @SuppressLint("ResourceType", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,24 +61,57 @@ class SelectNodeActivity : BaseCompatActivity() {
         cb1 = findViewById(R.id.cb1)
         cb2 = findViewById(R.id.cb2)
         cb3 = findViewById(R.id.cb3)
-        cb4 = findViewById(R.id.cb4)
+        //cb4 = findViewById(R.id.cb4)
 
         cb1.id = 0
         cb2.id = 1
         cb3.id = 2
-        cb4.id = 3
+        //cb4.id = 3
+
+        base1 = nodeList[0].url
+        base2 = nodeList[1].url
+        base3 = nodeList[2].url
+        //base4 = nodeList[3].url
+
+        initColor()
+
+        connect()
 
 
-        val nodeList = App.get().nodeList
+        cb1.setOnClickListener {
+            cb1.isChecked = true
+            grContent.check(-1)
+            connect()
+        }
+        cb2.setOnClickListener {
+            cb2.isChecked = true
+            grContent.check(-1)
+            connect()
+        }
+        cb3.setOnClickListener {
+            cb3.isChecked = true
+            grContent.check(-1)
+            connect()
+        }
+        /*cb4.setOnClickListener {
+            cb4.isChecked = true
+            grContent.check(-1)
+            connect()
+        }*/
 
-        val base1 = nodeList[0].url
-        val base2 = nodeList[1].url
-        val base3 = nodeList[2].url
-        val base4 = nodeList[3].url
+        grContent.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
+            override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+                LogUtils.i("SelectNodeActivity-checkedId", checkedId)
+                if (-1 == checkedId) run { return } else {
+                    ShareUtils.setString(Extras.SP_SELECTED_NODE, nodeList[checkedId].url)
+                }
+            }
+        })
+    }
 
-        var base = ShareUtils.getString(Extras.SP_SELECTED_NODE, base1)
+    private fun connect() {
 
-        var index = 1
+        val base = ShareUtils.getString(Extras.SP_SELECTED_NODE, base1)
 
         for (item in nodeList) {
             if (item.url == base) {
@@ -67,155 +123,157 @@ class SelectNodeActivity : BaseCompatActivity() {
             1 -> cb1.isChecked = true
             2 -> cb2.isChecked = true
             3 -> cb3.isChecked = true
-            4 -> cb4.isChecked = true
+            //4 -> cb4.isChecked = true
         }
 
-        var green = resources.getDrawable(R.drawable.icon_node_green)
-        green.setBounds(0, 0, green.minimumWidth, green.minimumHeight)
-        var yellow = resources.getDrawable(R.drawable.icon_node_yellow)
-        yellow.setBounds(0, 0, yellow.minimumWidth, yellow.minimumHeight)
-        var red = resources.getDrawable(R.drawable.icon_node_red)
-        red.setBounds(0, 0, red.minimumWidth, red.minimumHeight)
+        if (cb1.isChecked) {
+            text1 = "已连接"
+            text2 = ""
+            text3 = ""
+            //text4 = ""
+        } else if (cb2.isChecked) {
+            text1 = ""
+            text2 = "已连接"
+            text3 = ""
+            //text4 = ""
+        } else if (cb3.isChecked) {
+            text1 = ""
+            text2 = ""
+            text3 = "已连接"
+            //text4 = ""
+        } /*else if (cb4.isChecked) {
+            text1 = ""
+            text2 = ""
+            text3 = ""
+            text4 = "已连接"
+        }*/
 
         val createApi1 = Networking(App.get()).createApi(EthJsonRpcApiWithAuth::class.java, base1)
         val agent1 = EthsRpcAgent.by(createApi1)
 
         val startTime1 = System.currentTimeMillis()
         LogUtils.i("SelectNodeActivity-base1-time-start", startTime1)
-        agent1.checkNode().doMain().subscribe({
-            LogUtils.i("SelectNodeActivity", it)
-            LogUtils.i("SelectNodeActivity-base1-time-end", System.currentTimeMillis())
+        agent1.checkNode()
+                .io_main()
+                .subscribeBy(
+                        onSuccess = {
+                            var res: String
+                            if (System.currentTimeMillis() - startTime1 <= 300) {
+                                cb1.setCompoundDrawables(null, null, green, null)
+                                res = resources.getString(R.string.select_node_green)
+                            } else if (System.currentTimeMillis() - startTime1 <= 1000) {
+                                cb1.setCompoundDrawables(null, null, yellow, null)
+                                res = resources.getString(R.string.select_node_yellow)
+                            } else {
+                                cb1.setCompoundDrawables(null, null, red, null)
+                                res = resources.getString(R.string.select_node_red)
+                            }
+                            res = text1 + res
 
-            var res: String
-            if (System.currentTimeMillis() - startTime1 <= 300) {
-                cb1.setCompoundDrawables(null, null, green, null)
-                res = resources.getString(R.string.select_node_green)
-            } else if (System.currentTimeMillis() - startTime1 <= 1000) {
-                cb1.setCompoundDrawables(null, null, yellow, null)
-                res = resources.getString(R.string.select_node_yellow)
-            } else {
-                cb1.setCompoundDrawables(null, null, red, null)
-                res = resources.getString(R.string.select_node_red)
-            }
+                            cb1.text = spannableString(nodeList[0].name, res)
 
-            cb1.text = spannableString(nodeList[0].name, res)
-        }, {
-            LogUtils.e(it)
-            cb1.setCompoundDrawables(null, null, red, null)
-            cb1.text = spannableString(nodeList[0].name, resources.getString(R.string.select_node_red))
-        })
+                        },
+                        onError = {
+                            cb1.setCompoundDrawables(null, null, red, null)
+                            cb1.text = spannableString(nodeList[0].name, resources.getString(R.string.select_node_timeout))
+                        })
 
         val createApi2 = Networking(App.get()).createApi(EthJsonRpcApiWithAuth::class.java, base2)
         val agent2 = EthsRpcAgent.by(createApi2)
 
         val startTime2 = System.currentTimeMillis()
         LogUtils.i("SelectNodeActivity-base2-time-start", startTime2)
-        agent2.checkNode().doMain().subscribe({
-            LogUtils.i("SelectNodeActivity", it)
-            LogUtils.i("SelectNodeActivity-base2-time-end", System.currentTimeMillis())
+        agent2.checkNode()
+                .io_main()
+                .subscribeBy(
+                        onSuccess = {
+                            var res: String
+                            if (System.currentTimeMillis() - startTime2 <= 300) {
+                                cb2.setCompoundDrawables(null, null, green, null)
+                                res = resources.getString(R.string.select_node_green)
+                            } else if (System.currentTimeMillis() - startTime2 <= 1000) {
+                                cb2.setCompoundDrawables(null, null, yellow, null)
+                                res = resources.getString(R.string.select_node_yellow)
+                            } else {
+                                cb2.setCompoundDrawables(null, null, red, null)
+                                res = resources.getString(R.string.select_node_red)
+                            }
+                            res = text2 + res
 
-            var res: String
-            if (System.currentTimeMillis() - startTime2 <= 300) {
-                cb2.setCompoundDrawables(null, null, green, null)
-                res = resources.getString(R.string.select_node_green)
-            } else if (System.currentTimeMillis() - startTime2 <= 1000) {
-                cb2.setCompoundDrawables(null, null, yellow, null)
-                res = resources.getString(R.string.select_node_yellow)
-            } else {
-                cb2.setCompoundDrawables(null, null, red, null)
-                res = resources.getString(R.string.select_node_red)
-            }
-
-            cb2.text = spannableString(nodeList[1].name, res)
-
-        }, {
-            LogUtils.e(it)
-            cb2.setCompoundDrawables(null, null, red, null)
-            cb2.text = spannableString(nodeList[1].name, resources.getString(R.string.select_node_red))
-        })
+                            cb2.text = spannableString(nodeList[1].name, res)
+                        },
+                        onError = {
+                            cb2.setCompoundDrawables(null, null, red, null)
+                            cb2.text = spannableString(nodeList[1].name, resources.getString(R.string.select_node_timeout))
+                        })
         val createApi3 = Networking(App.get()).createApi(EthJsonRpcApiWithAuth::class.java, base3)
         val agent3 = EthsRpcAgent.by(createApi3)
 
         val startTime3 = System.currentTimeMillis()
         LogUtils.i("SelectNodeActivity-base3-time-start", startTime3)
-        agent3.checkNode().doMain().subscribe({
-            LogUtils.i("SelectNodeActivity", it)
-            LogUtils.i("SelectNodeActivity-base3-time-end", System.currentTimeMillis())
 
-            var res: String
-            if (System.currentTimeMillis() - startTime3 <= 300) {
-                cb3.setCompoundDrawables(null, null, green, null)
-                res = resources.getString(R.string.select_node_green)
-            } else if (System.currentTimeMillis() - startTime3 <= 1000) {
-                cb3.setCompoundDrawables(null, null, yellow, null)
-                res = resources.getString(R.string.select_node_yellow)
-            } else {
-                cb3.setCompoundDrawables(null, null, red, null)
-                res = resources.getString(R.string.select_node_red)
-            }
-            cb2.text = spannableString(nodeList[2].name, res)
-        }, {
-            LogUtils.e(it)
-            cb3.setCompoundDrawables(null, null, red, null)
-            cb3.text = spannableString(nodeList[2].name, resources.getString(R.string.select_node_red))
-        })
-        val createApi4 = Networking(App.get()).createApi(EthJsonRpcApiWithAuth::class.java, base4)
+        agent3.checkNode()
+                .io_main()
+                .subscribeBy(
+                        onSuccess = {
+                            var res: String
+                            if (System.currentTimeMillis() - startTime3 <= 300) {
+                                cb3.setCompoundDrawables(null, null, green, null)
+                                res = resources.getString(R.string.select_node_green)
+                            } else if (System.currentTimeMillis() - startTime3 <= 1000) {
+                                cb3.setCompoundDrawables(null, null, yellow, null)
+                                res = resources.getString(R.string.select_node_yellow)
+                            } else {
+                                cb3.setCompoundDrawables(null, null, red, null)
+                                res = resources.getString(R.string.select_node_red)
+                            }
+                            res = text3 + res
+
+                            cb3.text = spannableString(nodeList[2].name, res)
+                        },
+                        onError = {
+                            cb3.setCompoundDrawables(null, null, red, null)
+                            cb3.text = spannableString(nodeList[2].name, resources.getString(R.string.select_node_timeout))
+                        })
+
+        /*val createApi4 = Networking(App.get()).createApi(EthJsonRpcApiWithAuth::class.java, base4)
         val agent4 = EthsRpcAgent.by(createApi4)
 
         val startTime4 = System.currentTimeMillis()
         LogUtils.i("SelectNodeActivity-base4-time-start", startTime4)
-        agent4.checkNode().doMain().subscribe({
-            LogUtils.i("SelectNodeActivity", it)
-            LogUtils.i("SelectNodeActivity-base4-time-end", System.currentTimeMillis())
 
-            var res: String
-            if (System.currentTimeMillis() - startTime4 <= 300) {
-                cb4.setCompoundDrawables(null, null, green, null)
-                res = resources.getString(R.string.select_node_green)
-            } else if (System.currentTimeMillis() - startTime4 <= 1000) {
-                cb4.setCompoundDrawables(null, null, yellow, null)
-                res = resources.getString(R.string.select_node_yellow)
-            } else {
-                cb4.setCompoundDrawables(null, null, red, null)
-                res = resources.getString(R.string.select_node_red)
-            }
-            cb4.text = spannableString(nodeList[3].name, res)
-        }, {
-            LogUtils.e(it)
-            cb4.setCompoundDrawables(null, null, red, null)
-            cb4.text = spannableString(nodeList[3].name, resources.getString(R.string.select_node_red))
-        })
+        agent4.checkNode()
+                .io_main()
+                .subscribeBy(
+                        onSuccess = {
+                            var res: String
+                            if (System.currentTimeMillis() - startTime4 <= 300) {
+                                cb4.setCompoundDrawables(null, null, green, null)
+                                res = resources.getString(R.string.select_node_green)
+                            } else if (System.currentTimeMillis() - startTime4 <= 1000) {
+                                cb4.setCompoundDrawables(null, null, yellow, null)
+                                res = resources.getString(R.string.select_node_yellow)
+                            } else {
+                                cb4.setCompoundDrawables(null, null, red, null)
+                                res = resources.getString(R.string.select_node_red)
+                            }
+                            res = text4 + res
 
-        cb1.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                grContent.check(-1)
-            }
-        }
-        cb2.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                grContent.check(-1)
-            }
-        }
-        cb3.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                grContent.check(-1)
-            }
-        }
-        cb4.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                grContent.check(-1)
-            }
-        }
+                            cb4.text = spannableString(nodeList[3].name, res)
+                        },
+                        onError = {
+                            cb4.setCompoundDrawables(null, null, red, null)
+                            cb4.text = spannableString(nodeList[3].name, resources.getString(R.string.select_node_timeout))
+                        })*/
+    }
 
-        grContent.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
-            override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
-                LogUtils.i("SelectNodeActivity-checkedId", checkedId)
-                if (-1 == checkedId) run { return } else {
-                    ShareUtils.setString(Extras.SP_SELECTED_NODE, nodeList[checkedId].url)
-                }
-            }
-        })
-
+    private fun initColor() {
+        green = resources.getDrawable(R.drawable.icon_node_green)
+        green.setBounds(0, 0, green.minimumWidth, green.minimumHeight)
+        yellow = resources.getDrawable(R.drawable.icon_node_yellow)
+        yellow.setBounds(0, 0, yellow.minimumWidth, yellow.minimumHeight)
+        red = resources.getDrawable(R.drawable.icon_node_red)
+        red.setBounds(0, 0, red.minimumWidth, red.minimumHeight)
     }
 
     private fun spannableString(name: String, res: String): SpannableString {
@@ -232,4 +290,5 @@ class SelectNodeActivity : BaseCompatActivity() {
         App.get().initData(App.get())
 
     }
+
 }
