b9ef99ecfc622bf8a07ca7a1ff9c567998829579
Andyyang2014 <andyyang2014@126.com>
2018-12-03 19:26:11 +0800

    userinfo

--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/cashloan/act/UserInfoCertificationActivity.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/cashloan/act/UserInfoCertificationActivity.kt
@@ -1,29 +1,60 @@
 package io.wexchain.android.dcc.modules.cashloan.act
 
 import android.app.Dialog
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.view.ViewCompat
+import android.support.v7.widget.RecyclerView
 import android.view.*
-import io.wexchain.android.common.base.BaseCompatActivity
+import io.wexchain.android.common.base.BindActivity
+import io.wexchain.android.common.getViewModel
+import io.wexchain.android.common.onClick
+import io.wexchain.android.dcc.view.adapter.ItemViewClickListener
+import io.wexchain.android.dcc.view.adapter.SimpleDataBindAdapter
+import io.wexchain.dcc.BR
 import io.wexchain.dcc.R
+import io.wexchain.dcc.databinding.ActivityUserinfoCertificationBinding
+import io.wexchain.dcc.databinding.DialogListTagBinding
+import io.wexchain.dcc.databinding.ItemTagBinding
 
 /**
  *Created by liuyang on 2018/10/17.
  */
-class UserInfoCertificationActivity : BaseCompatActivity() {
+class UserInfoCertificationActivity : BindActivity<ActivityUserinfoCertificationBinding>() {
 
-    private val MarriageList = listOf("未婚", "已婚", "离异")
-    private val LoanPurposeList = listOf("房屋支出", "看病就医", "家装指出", "旅游指出", "网购消费")
-    private val WorkCategoryList = listOf("工薪", "企业主", "个体户", "自由职业")
-    private val RelationList = listOf("父亲", "母亲", "儿子", "女儿", "兄弟", "姐妹", "配偶", "同学", "亲戚", "同事", "朋友", "其他")
-    private val WorkIndustryList = listOf("商业、服务人员", "专业技术人员", "办事人员、文员、行政等", "工业、生产、运输人员", "农、林、牧、渔、水利业人员",
+    override val contentLayoutId: Int
+        get() = R.layout.activity_userinfo_certification
+
+    private var bankDialog: Dialog? = null
+
+    private val MarriageList = arrayListOf("未婚", "已婚", "离异")
+    private val LoanPurposeList = arrayListOf("房屋支出", "看病就医", "家装指出", "旅游指出", "网购消费")
+    private val WorkCategoryList = arrayListOf("工薪", "企业主", "个体户", "自由职业")
+    private val RelationList = arrayListOf("父亲", "母亲", "儿子", "女儿", "兄弟", "姐妹", "配偶", "同学", "亲戚", "同事", "朋友", "其他")
+    private val WorkIndustryList = arrayListOf("商业、服务人员", "专业技术人员", "办事人员、文员、行政等", "工业、生产、运输人员", "农、林、牧、渔、水利业人员",
             "前线销售人员", "国际机关、企事业单位管理人员", "军人")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_userinfo_certification)
         initToolbar()
+        initVm()
+        initClick()
+    }
+
+    private fun initClick() {
+        binding.marriagesTatus.onClick {
+            showTagDialog(MarriageList) {
+                binding.marriagesTatus.text = it
+            }
+        }
+    }
+
+    private fun initVm() {
+        binding.asAddressVm = getViewModel("asAddressVm")
+        binding.asEnterpriseVm = getViewModel("asEnterpriseVm")
+        binding.asAddressVm!!.init(this)
+        binding.asEnterpriseVm!!.init(this)
     }
 
     override fun onStop() {
@@ -35,32 +66,39 @@ class UserInfoCertificationActivity : BaseCompatActivity() {
 //        CertificationInfo()
     }
 
-    class ListTagDialog :DialogFragment(){
 
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-            val view = inflater.inflate(R.layout.dialog_list_tag, container, false)
-            ViewCompat.setElevation(view, resources.getDimension(R.dimen.dialog_elevation))
-            return view
-        }
+    fun showTagDialog(list: ArrayList<String>, data: (String) -> Unit) {
+       /* val dialog = ListTagDialog.create(list)
+        dialog.show(supportFragmentManager, null)
+        dialog.onItemClick = data*/
 
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            setStyle(STYLE_NORMAL, R.style.Theme_AppCompat_Light_Dialog_Alert_Dcc)
-            val dialog = super.onCreateDialog(savedInstanceState)
-            dialog.window.apply {
-                setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM)
-                setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        val adapter = SimpleDataBindAdapter<ItemTagBinding, String>(
+                layoutId = R.layout.item_tag,
+                variableId = BR.name,
+                itemViewClickListener = object : ItemViewClickListener<String> {
+                    override fun onItemClick(item: String?, position: Int, viewId: Int) {
+                        data(item!!)
+                        bankDialog?.dismiss()
+                    }
+                }
+        )
+        adapter.setList(list)
+
+        var dialog = bankDialog
+        if (dialog == null) {
+            dialog = Dialog(this,R.style.FullWidthWhiteDialog).apply {
+                setContentView(R.layout.dialog_list_tag)
+                window.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM)
+                window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+                this.findViewById<RecyclerView>(R.id.rv_tag)!!.adapter = adapter
             }
-            return dialog
+            bankDialog = dialog
         }
-
-        private var host: UserInfoCertificationActivity? = null
-
-        companion object {
-            fun create(activity: UserInfoCertificationActivity): ListTagDialog {
-                val dialog = ListTagDialog()
-                dialog.host = activity
-                return dialog
-            }
+        if (dialog.isShowing) {
+            dialog.dismiss()
+        } else {
+            dialog.show()
         }
     }
+
 }
---
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/cashloan/vm/AddressVm.kt
@@ -0,0 +1,40 @@
+package io.wexchain.android.dcc.modules.cashloan.vm
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.ViewModel
+import android.content.Context
+import android.databinding.ObservableField
+import io.wexchain.android.dcc.tools.PickerHelper
+
+/**
+ *Created by liuyang on 2018/12/3.
+ */
+class AddressVm : ViewModel() {
+
+    private val helper = PickerHelper()
+
+    fun init(context: Context){
+        helper.init(context)
+    }
+
+    val province = ObservableField<String>()
+            .apply { set("选择省份") }
+    val city = ObservableField<String>()
+            .apply { set("选择城市") }
+    val area = ObservableField<String>()
+            .apply { set("选择地区") }
+
+
+    fun selectAddress() {
+        helper.showPickerView { p, c, a ->
+            province.set(p)
+            city.set(c)
+            area.set(a)
+        }
+    }
+
+    fun isSelect(): Boolean {
+        return !(province.get() == "选择省份" || city.get() == "选择城市" || area.get() == "选择地区")
+    }
+}
