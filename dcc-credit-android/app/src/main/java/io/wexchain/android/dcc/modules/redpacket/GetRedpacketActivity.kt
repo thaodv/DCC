4cade9a73198cd01e874e2bf1d927cad790c2037
zgjsntwangpeng <zgjsntwangpeng@sina.cn>
2018-12-19 18:44:26 +0800

    redpacket

--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/redpacket/GetRedpacketActivity.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/redpacket/GetRedpacketActivity.kt
@@ -1,13 +1,23 @@
 package io.wexchain.android.dcc.modules.redpacket
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import io.wexchain.android.common.base.BindActivity
+import io.wexchain.android.common.navigateTo
 import io.wexchain.dcc.R
+import io.wexchain.dcc.databinding.ActivityGetRedpacketBinding
 
-class GetRedpacketActivity : AppCompatActivity() {
+class GetRedpacketActivity : BindActivity<ActivityGetRedpacketBinding>() {
+
+    override val contentLayoutId: Int get() = R.layout.activity_get_redpacket
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_redpacket)
+        initToolbar(true)
+
+        binding.tvRule.setOnClickListener {
+            navigateTo(RuleActivity::class.java)
+        }
+
+
     }
 }
--- dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/redpacket/InviteRecordActivity.kt
+++ dcc-credit-android/app/src/main/java/io/wexchain/android/dcc/modules/redpacket/InviteRecordActivity.kt
@@ -1,13 +1,14 @@
 package io.wexchain.android.dcc.modules.redpacket
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import io.wexchain.android.common.base.BaseCompatActivity
 import io.wexchain.dcc.R
 
-class InviteRecordActivity : AppCompatActivity() {
+class InviteRecordActivity : BaseCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_invite_record)
+        initToolbar(true)
     }
 }
